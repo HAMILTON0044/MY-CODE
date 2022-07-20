3c80d8e22b18a9b4df73d1b21f078b4b804dc97e
HAMILTON0044 <109563920+HAMILTON0044@users.noreply.github.com>
2022-07-20 09:03:07 +0800

    Add files via upload

---
+++ Project2/Project2/源.cpp
@@ -0,0 +1,15 @@
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char*));//4
+//	printf("%d\n", sizeof(double*));//4
+//	return 0;
+//}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	return 0;
+}
