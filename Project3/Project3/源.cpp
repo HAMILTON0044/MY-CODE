40eb9e8c8fd12200c9de9eb2df123feaf9b0d07c
HAMILTON0044 <109563920+HAMILTON0044@users.noreply.github.com>
2022-07-21 08:59:45 +0800

    Add files via upload

---
+++ Project3/Project3/源.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int age = 0;
+//	printf("age=\n");
+//	scanf("%d",&age);
+//	if (age < 18)
+//		printf("你还敢喝酒？\n");
+//	else if (age >= 18 && age <= 28)
+//		printf("小青年\n");
+//	else if (age > 28 && age <= 50)
+//		printf("壮年\n");
+//	else if (age > 20 && age <= 80)
+//		printf("老人\n");
+//	else if (age > 80 )
+//		printf("仙人\n");
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+	{
+		if (b == 2)
+			printf("hehe");
+		else
+			printf("haha");
+	}
+	else
+		printf("F**K");
+	return 0;
+}
