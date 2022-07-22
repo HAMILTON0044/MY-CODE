0ecedcf66aaee9f345b1adc6ede0b83f253dcc1c
HAMILTON0044 <109563920+HAMILTON0044@users.noreply.github.com>
2022-07-22 10:07:07 +0800

    Add files via upload

---
+++ Project4/Project4/源.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 999;
+//	int c = 0;
+//    scanf("%d", &a);
+//	c = a + b;
+//	printf("%d", c);
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//         if (i % 2 == 1)
+//		 printf("%d\n", i);
+//	     i++;
+//	}
+//        
+//	return 0;
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("老实工作吧");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息");
+//        break;
+//	default:
+//		printf("你不老实");
+//		break;
+//    }
+//	return 0;
+//}
+
+//break 可使循环终止  continue 本次循环 跳过本次循环剩余部分进行下一次循环
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
