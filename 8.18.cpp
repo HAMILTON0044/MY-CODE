4d04b63da9b39e0d449cc4e163355e185abe6893
HAMILTON0044 <109563920+HAMILTON0044@users.noreply.github.com>
2022-08-23 07:27:07 +0800

    Add files via upload

---
+++ 8.18.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/*int main()
+{
+	int a=5/2;
+	printf("%d",a);
+	return 0;
+} 
+int main()
+{
+	int a=-1;
+	int b=a>>1;
+	printf("%d",b);
+	return 0;
+}*/
+int main()
+{
+	int a=3;
+	int b=5;
+	int c=a|b;
+	printf("%d",c);
+	return 0;
+	//|
+	//00000000000000000000000000000011
+	//00000000000000000000000000000101
+	
+	//00000000000000000000000000000111
+	
+	
+} 
